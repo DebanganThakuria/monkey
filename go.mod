9793ad4afcc7efb763e4246af214883a8f7e9d58
DebanganThakuria <debangan.thakuria@razorpay.com>
2023-06-17 11:58:43 +0530

    Update module name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/procodr/monkey
+module github.com/DebanganThakuria/monkey
 
 go 1.13
--- monkey_arm64.go
+++ monkey_arm64.go
@@ -1,6 +1,5 @@
 package monkey
 
-
 import "unsafe"
 
 func jmpToFunctionValue(double uintptr) []byte {
@@ -10,10 +9,10 @@ func jmpToFunctionValue(double uintptr) []byte {
 	d4d5 := double >> 32 & 0xFFFF
 	d6d7 := double >> 48 & 0xFFFF
 
-	res = append(res, movImm(0B10, 0, d0d1)...)          // MOVZ x26, double[16:0]
-	res = append(res, movImm(0B11, 1, d2d3)...)          // MOVK x26, double[32:16]
-	res = append(res, movImm(0B11, 2, d4d5)...)          // MOVK x26, double[48:32]
-	res = append(res, movImm(0B11, 3, d6d7)...)          // MOVK x26, double[64:48]
+	res = append(res, movImm(0b10, 0, d0d1)...)          // MOVZ x26, double[16:0]
+	res = append(res, movImm(0b11, 1, d2d3)...)          // MOVK x26, double[32:16]
+	res = append(res, movImm(0b11, 2, d4d5)...)          // MOVK x26, double[48:32]
+	res = append(res, movImm(0b11, 3, d6d7)...)          // MOVK x26, double[64:48]
 	res = append(res, []byte{0x4A, 0x03, 0x40, 0xF9}...) // LDR x10, [x26]
 	res = append(res, []byte{0x40, 0x01, 0x1F, 0xD6}...) // BR x10
 
